e8f92cab09a68680a8f0c24399aa3f50f8a55ede
ella_ent <entella19@gmail.com>
2022-06-28 12:31:18 +0300

    planeks

--- csv_generator/urls.py
+++ csv_generator/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
         FileDownloadView.as_view(),
         name="dataset_download",
     ),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
